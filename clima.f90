d9c5d99331922be2689d7fa449071a3aa92cb667
Talita Ventura <118145909+TalitaVentura16@users.noreply.github.com>
2023-05-31 14:20:31 -0300

    Add files via upload

---
+++ clima.f90
@@ -0,0 +1,26 @@
+read (10, file = xxx) pentada
+read(20, file = yyy) pentclim
+
+do k = 1, NY
+    icont_i = 0
+    icont_f = 0
+    do i = 1, NP (número de pentadas total 365 / 5)
+        do j = i, i+8
+            if (pentada(i). lt. pentclim(i)) icont_i = icont_i + 1
+            if (pentada(i).gt.pentclim(i)) icont_f = icont_f + 1
+        enddo
+        if(icont_i.ge.6) then
+            kpi = k * i
+            write(20,*) k, i, kpi(k)
+        endif
+        if(icont_f.ge.6)then
+            kpf = k * i
+            write(30,*) k, i, kpf(k)
+        endif
+    enddo
+
+    do k = 1, NY
+        dsl = kpf (k) - kpi (k)
+        write (40,*) k, dsl
+    enddo
+enddo
