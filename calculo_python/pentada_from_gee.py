3e3f1db359b9e549a439e51ebff8404fc4a043a9
Talitaventura16 <Talitaventura16@outlook.com>
2023-08-28 17:26:22 -0300

    O programa foi finalizado, a função retorna os dados de chuva acumulado de pêntada, para o período definido, e salva as datas e valores em excel. Por enquanto, aguardo orintações de minha professora orientadora para futuras modificações e criação de novas funcionalidades.

--- calculo_python/pentada_from_gee.py
+++ calculo_python/pentada_from_gee.py
@@ -8,60 +8,48 @@ import ee
 import pandas as pd
 from datetime import datetime, timedelta
 
-def calcula_pentada(dataInicio, dataFim, coordenadas):
+def calcula_pentada(dataInicio, dataFim):
+    import ee
+
     # Autenticação e inicialização da API
     ee.Authenticate()
     ee.Initialize()
-    
-    # Definir intervalo de tempo
-    intervalo = 5
 
-    # Base utlizada para a coleta de dados
-    # Grupo de Riscos Climaticos Precipitacao por infravermelho com dados da estacao (CHIRPS).
-    satelite = ee.ImageCollection("UCSB-CHG/CHIRPS/DAILY")
 
-    # Base de extracao
-    regiao = ee.Geometry.Rectangle(coordenadas)
-    dadosFiltrados = satelite.filterBounds(regiao).filterDate(dataInicio, dataFim)
-    bandasSelecionadas = dadosFiltrados.select(["precipitation"])
-    
-    # Definir listas para armazenar datas e acumulações
-    datas = []
-    acumulados = []
-    
-    # Iterar sobre as datas para calcular o acumulado
-    data_corrente = dataInicio
-    while data_corrente <= dataFim:
-        data_final_intervalo = data_corrente + timedelta(days=intervalo)
-        
-        # Filtrar as imagens dentro do intervalo
-        imagens_intervalo = bandasSelecionadas.filterDate(data_corrente, data_final_intervalo)
-        
-        # Calcular a soma das imagens do intervalo
-        acumulado = imagens_intervalo.sum().reduceRegion(
-            reducer=ee.Reducer.sum(),
-            geometry=regiao,
-            scale=5000
-        ).get("precipitation")
-        
-        # Adicionar a data e o acumulado às listas
-        datas.append(data_corrente.strftime("%d-%m-%Y"))
-        acumulados.append(acumulado.getInfo())
-        
-        data_corrente = data_final_intervalo + timedelta(days=1)
-    
-    # Criar um DataFrame com as datas e acumulados
-    df = pd.DataFrame({"Data": datas, "Acumulado de Precipitação (mm)": acumulados})
-    
-    return df
+    # Base utilizada para a coleta de dados
+    # A base escolhida  é a CHIRPS - Grupo de Riscos Climaticos Precipitacao, os dados ja sao gerados com o intervado da
+    # pentada.
+    dataset = ee.ImageCollection('UCSB-CHG/CHIRPS/PENTAD') \
+        .filterDate(dataInicio, dataFim)
 
-# Local de estudo, as coordenadas devem estar em WGS 84 (latitude e longitude em graus decimais)
-coordenadasAmazonia = [-70.1383, -9.7615, -55.9734, -3.4653] 
+    # Selecionar a banda de precipitacao
+    precipitation = dataset.select('precipitation')
+
+    # Obter informacoes das imagens e armazena em uma lista
+    image_list = precipitation.toList(precipitation.size())
+
+    # Percorrer a lista de imagens e obter as datas e valores de precipitacao
+    datasValores = []
+    for i in range(image_list.size().getInfo()):
+        image = ee.Image(image_list.get(i))
+        dado = ee.Date(image.get('system:time_start')).format('yyyy-MM-dd').getInfo()
+        valor = image.reduceRegion(ee.Reducer.sum(), geometry=ee.Geometry.Point([-64.02760881257484,-4.436083218695817])).get('precipitation').getInfo()
+        datasValores.append((dado, valor))
+
+    # Imprimi as datas e valores
+    for dado, valor in datasValores:
+        print(f"{dado}, {valor}")
+
+    return datasValores
 
 # Definir datas de inicio e fim 
 dataInicio = datetime.strptime("01-01-2014", "%d-%m-%Y")
 dataFim = dataInicio + timedelta(days=306)
 
 # Chamar a função para calcular a pentada
-df_pentada = calcula_pentada(dataInicio, dataFim, coordenadasAmazonia)
-print(df_pentada)
+df_pentada = calcula_pentada(dataInicio, dataFim)
+valoresPentada = pd.DataFrame(df_pentada, columns=["Data", "Valor de Precipitação"])
+print(valoresPentada)
+
+# Salvar o DataFrame no arquivo Excel
+valoresPentada.to_excel('pentada_amazonia.xlsx', index=False)
